00c56edae26979ced090000121c0ba908832e56d
masarad <masaradorothy@gmail.com>
2023-03-16 08:36:33 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that does not use any function listed in the NAME section
+ * Return: 1 (Success)
+ */
+ int main(void)
+ {
+	 writes("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+ return (1);
+ }
