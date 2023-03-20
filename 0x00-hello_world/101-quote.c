3bca516133a3592c1640616302c64c0464e3ed23
masarad <masaradorothy@gmail.com>
2023-03-20 06:34:07 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that does not use any function listed in the NAME section
- * Return: 1 (Success)
+ * main - A C program that leads to the standard error
+ * Return: 0 (Success)
  */
  int main(void)
  {
-	 writes("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
  return (1);
+
  }
+
+
