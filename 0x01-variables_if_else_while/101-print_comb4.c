5a9b2c421b5ad17993486f5346a5ea5c758c6785
masarad <masaradorothy@gmail.com>
2023-03-22 00:56:57 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,27 +3,31 @@
 #include <time.h>
 
 /**
- * main - A program that prints combinations of 2 digits
+ * main - A program that prints combinations of three digits
  * Return: 0 (Success)
  */
 
 int main(void)
 
 {
-	int d, p;
+	int d, p, q;
 
 	for (d = '0'; d < '9'; d++)
+	{
 
 	for (p = d + 1; p <= '9'; p++)
 
-	if (p != d)
+	for (q = p + 1; q <= '9'; q++)
+
+	if ((p != d) != q)
 	putchar(d);
 	putchar(p);
-	if (d == '8' && p == '9')
+	putchar(q);
+	if (d == '7' && p == '8')
 	putchar(',');
 	putchar(' ');
 	putchar('\n');
+	}
 
 	return (0);
-
 }
