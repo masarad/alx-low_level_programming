343a1d2324720f59c4a1dc4ce2256a1ae5060097
masarad <masaradorothy@gmail.com>
2023-04-05 02:50:09 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Function that prints a string
+ * @s: Input
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
