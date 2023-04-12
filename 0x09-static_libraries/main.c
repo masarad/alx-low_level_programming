6908f201a3a6b9c9e79115ade5eef30e605d0cd7
masarad <masaradorothy@gmail.com>
2023-04-12 02:24:18 -0700

    Task 0

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Check the code
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
